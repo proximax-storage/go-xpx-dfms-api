252b76f65a345150182f08a0dd6027d5e296e254
Vadim Didenko <52422557+vadimdidenkogs@users.noreply.github.com>
2019-10-31 21:18:46 +0200

    Update contract type (#1)
    * Update contract type

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/proximax-storage/go-xpx-dfms-api
 go 1.13
 
 require (
-	github.com/ipfs/go-ipfs-files v0.0.6 // indirect
-	github.com/proximax-storage/go-xpx-dfms-drive v0.1.0 // indirect
+	github.com/ipfs/go-ipfs-files v0.0.6
+	github.com/libp2p/go-libp2p-core v0.2.2
+	github.com/multiformats/go-multiaddr v0.1.1
+	github.com/proximax-storage/go-xpx-dfms-drive v0.1.4-0.20191031122255-6dac87c61890
 )
--- opts.go
+++ opts.go
@@ -40,7 +40,41 @@ func ParseDriveOptions(opts ...DriveOption) *DriveOptions {
 }
 
 type ComposeOpts struct {
-	Limit int
+	MinReplicators   uint16
+	PercentApprovers uint8
+	BillingPeriod    int64
+	BillingPrice     int64
 }
 
 type ComposeOpt func(*ComposeOpts)
+
+func MinReplicators(minReplicators uint16) ComposeOpt {
+	return func(opts *ComposeOpts) {
+		opts.MinReplicators = minReplicators
+	}
+}
+
+func PercentApprovers(percentApprovers uint8) ComposeOpt {
+	return func(opts *ComposeOpts) {
+		opts.PercentApprovers = percentApprovers
+	}
+}
+
+func BillingPrice(billingPrice int64) ComposeOpt {
+	return func(opts *ComposeOpts) {
+		opts.BillingPrice = billingPrice
+	}
+}
+func BillingPeriod(billingPeriod int64) ComposeOpt {
+	return func(opts *ComposeOpts) {
+		opts.BillingPeriod = billingPeriod
+	}
+}
+
+// Apply applies the given options to this DiscoveryOpts
+func (opts *ComposeOpts) Apply(options ...ComposeOpt) error {
+	for _, o := range options {
+		o(opts)
+	}
+	return nil
+}
