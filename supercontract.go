06630e83a6cd92f8782550a39d7bdac5036bf167
Evgeny Ukhanov <mrlsd@ya.ru>
2020-01-19 16:03:48 +0200

    Change SC deploy interface

--- supercontract.go
+++ supercontract.go
@@ -3,9 +3,8 @@ package api
 import (
 	"context"
 	idrive "github.com/proximax-storage/go-xpx-dfms-drive"
-	sc "github.com/proximax-storage/go-xpx-dfms-drive/supercontract"
 )
 
 type SuperContract interface {
-	Deploy(ctx context.Context, id idrive.ID, file string, functions []*sc.Function) error
+	Deploy(ctx context.Context, id idrive.ID, file string) error
 }
