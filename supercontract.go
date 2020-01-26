f5f61e0adcde833b6b7ba6578e5aa38fe57ca23d
Evgeny Ukhanov <mrlsd@ya.ru>
2020-01-26 15:24:38 +0200

    Fix SC interface

--- supercontract.go
+++ supercontract.go
@@ -11,5 +11,5 @@ type SuperContract interface {
 	Deploy(ctx context.Context, id idrive.ID, file string) error
 	Execute(ctx context.Context, id sc.ID, gas uint64, function string, functionParams []int64) error
 
-	GetSuperContract(context.Context, sc.ID) (*sc.SuperContract, err)
+	GetSuperContract(context.Context, sc.ID) (*sc.SuperContract, error)
 }
