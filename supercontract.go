f9264163b1a5c4b427ef9d0b934d82021f6dbbb1
Evgeny Ukhanov <mrlsd@ya.ru>
2020-01-26 15:13:15 +0200

    Extend SC: Execute, GetSuperContract

--- supercontract.go
+++ supercontract.go
@@ -2,9 +2,14 @@ package api
 
 import (
 	"context"
+
 	idrive "github.com/proximax-storage/go-xpx-dfms-drive"
+	sc "github.com/proximax-storage/go-xpx-dfms-drive/supercontract"
 )
 
 type SuperContract interface {
 	Deploy(ctx context.Context, id idrive.ID, file string) error
+	Execute(ctx context.Context, id sc.ID, gas uint64, function string, functionParams []int64) error
+
+	GetSuperContract(context.Context, sc.ID) (*sc.SuperContract, err)
 }
