3cb6177402ca992865a146217b9f6bbf256da98a
Yevhen <iorgachov1998@gmail.com>
2021-01-15 17:29:56 +0200

    add Node interface for common apis

--- apis.go
+++ apis.go
@@ -1,15 +1,19 @@
 package api
 
+type Node interface {
+	// Network returns implementation of Network api
+	Network() Network
+}
+
 // api.Client is scope of different apis available for DFMS Client node
 type Client interface {
+	Node
+
 	// Contract return implementation of ContractClient api
 	Contract() ContractClient
 
 	// FS return implementation of DriveFS api
 	FS() DriveFS
-
-	// Network returns implementation of Network api
-	Network() Network
 
 	// SuperContract return implementation of SuperContract api
 	SuperContract() SuperContract
@@ -17,9 +21,8 @@ type Client interface {
 
 // api.Replicator is scope of different apis available for DFMS Replicator node
 type Replicator interface {
+	Node
+
 	// Contract return implementation of ContractReplicator api
 	Contract() ContractReplicator
-
-	// Network returns implementation of Network api
-	Network() Network
 }
