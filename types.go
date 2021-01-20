b5a8cd2e6b4f261491fb4cace8ebea392376df9e
Yevhen <iorgachov1998@gmail.com>
2021-01-20 11:21:49 +0200

    expand Version struct with new fields

--- types.go
+++ types.go
@@ -41,35 +41,59 @@ func (res *VerifyResult) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-// NewVersion creates new version with current API and wanted Build
-func NewVersion(buildVersion string) *Versions {
-	return &Versions{
-		API:   apiVersion,
-		Build: buildVersion,
+// NewVersion creates new Version and fill all fields
+func NewVersion(build, commit, date, system, gv string) *Version {
+	return &Version{
+		API:       apiVersion,
+		Build:     build,
+		Commit:    commit,
+		BuildDate: date,
+		System:    system,
+		GO:        gv,
 	}
 }
 
-type Versions struct {
-	// Current version of API
+type Version struct {
+	// Current API version
 	API string
 
-	// Current version of app build
+	// Current Build version
 	Build string
+
+	// Commit hash
+	Commit string
+
+	// Data of node build
+	BuildDate string
+
+	// System for which the build was made
+	System string
+
+	// Version of Golang
+	GO string
 }
 
-func (v Versions) MarshalJSON() ([]byte, error) {
+func (v Version) Marshal() ([]byte, error) {
 	return json.Marshal(v)
 }
 
-func (v *Versions) UnmarshalJSON(data []byte) error {
+func (v *Version) Unmarshal(data []byte) error {
 	return json.Unmarshal(data, v)
 }
 
-func (v *Versions) String() string {
+func (v *Version) String() string {
 	return fmt.Sprintf(
 		"API Version: %s\n"+
-			"Build Version: %s\n",
+			"Build Version: %s\n"+
+			"Build date: %s\n"+
+			"Commit: %s\n"+
+			"System: %s\n"+
+			"Golang version: %s\n",
 		v.API,
 		v.Build,
+		v.Commit,
+		v.BuildDate,
+		v.System,
+		v.GO,
 	)
 }
