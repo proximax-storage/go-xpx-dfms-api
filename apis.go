ef258f229a5d6e8e18151324f6586d93719b1988
Viktor.Markvardt <viktor.markvardt@gmail.com>
2021-01-21 19:33:28 +0200

    Moved Ledger interface to separate file

--- apis.go
+++ apis.go
@@ -2,15 +2,7 @@ package api
 
 import (
 	"context"
-
-	drive "github.com/proximax-storage/go-xpx-dfms-drive"
 )
-
-type Ledger interface {
-	// ListDrives returns all active Drives registered on the Ledger.
-	// Note: Pagination will be introduced later on.
-	ListDrives(context.Context) ([]drive.ID, error)
-}
 
 type Node interface {
 	//Ledger returns implementation of Ledger api
